1b0dbb8e5ec6201aacf4b6b4d76821132a699558
Tony <tonyfrank@rambler.ru>
2020-04-21 19:00:18 +0400

    add new libs

--- gulpfile.js
+++ gulpfile.js
@@ -29,9 +29,8 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 });
 
 gulp.task('scripts', function() {
-	return gulp.src([ // Берем все необходимые библиотеки
-		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
-		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+	return gulp.src([ // Берем все необходимые библиотеки(сколько нужно)
+		'app/libs/bootstrap-4.3.1/dist/js/bootstrap.min.js', // Берем jQuery
 		])
 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		.pipe(uglify()) // Сжимаем JS файл
